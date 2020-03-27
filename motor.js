8959796b769a3938ea4ffad5838546b7a7dbfc43
margaritageleta <noticiasmundiales99@gmail.com>
2020-03-28 00:27:23 +0100

    tripadvisor. Thats all

---
+++ motor.js
@@ -0,0 +1,17 @@
+function copy() {
+    var copyText = document.getElementById("copiame");
+    var textArea = document.createElement("textarea");
+    textArea.value = copyText.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("Copy");
+    textArea.remove();
+
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+function scrollTo(hash) {
+    location.hash = "#" + hash;
+}
